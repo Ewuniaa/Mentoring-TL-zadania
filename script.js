5b810640dd0c1bbf13130da1750794a1f2080f3f
Ewuniaa <ewaa138@gmail.com>
2025-04-02 09:36:27 +0200

    zadanie 2 .Szyfr Cezara

---
+++ script.js
@@ -0,0 +1,22 @@
+function encriptar(text, key) {
+  let encriptedText = "";
+  for (let i = 0; i < text.length; i++) {
+    let symbol = text[i];
+    if (symbol.match(/[a-z]/i)) {
+      let Code = symbol.charCodeAt(0);
+      let base = symbol === symbol.toUpperCase() ? 65 : 97;
+      let newCode = ((Code - base + key) % 26) + base;
+
+      encriptedText += String.fromChartCode(newCode);
+    } else {
+      encriptedText += symbol;
+    }
+  }
+  return encriptedText;
+}
+
+let text = "Hello World";
+let key = 3;
+let encriptedText = encriptar(text, key);
+console.log(encripedText);
+// Output; "Khoor Zruog"
